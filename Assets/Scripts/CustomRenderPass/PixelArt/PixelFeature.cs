900736e1d9350d37f1899f9045e03bcc9d6420a8
KurumiChiew <64896661+chiew12301@users.noreply.github.com>
2024-10-02 21:08:46 +0800

    Change settings

--- Assets/Scripts/CustomRenderPass/PixelArt/PixelFeature.cs
+++ Assets/Scripts/CustomRenderPass/PixelArt/PixelFeature.cs
@@ -1,31 +1,31 @@
-using UnityEngine.Rendering.Universal;
+//using UnityEngine.Rendering.Universal;
 
-namespace UnityEngine.Experimental.Rendering.Universal
-{
-    public class PixelFeature : ScriptableRendererFeature
-    {
-        [System.Serializable]
-        public class PixelFeatureSettings
-        {
-            public LayerMask layerMask = 0;
-            public RenderPassEvent evt = RenderPassEvent.BeforeRenderingTransparents;
-            public Material matBlit = null;
-            [Range(1.0f, 15.0f)]
-            public float pixelDensity = 1.0f;
-        }
+//namespace UnityEngine.Experimental.Rendering.Universal
+//{
+//    public class PixelFeature : ScriptableRendererFeature
+//    {
+//        [System.Serializable]
+//        public class PixelFeatureSettings
+//        {
+//            public LayerMask layerMask = 0;
+//            public RenderPassEvent evt = RenderPassEvent.BeforeRenderingTransparents;
+//            public Material matBlit = null;
+//            [Range(1.0f, 15.0f)]
+//            public float pixelDensity = 1.0f;
+//        }
 
-        public PixelFeatureSettings settings = new PixelFeatureSettings();
+//        public PixelFeatureSettings settings = new PixelFeatureSettings();
 
-        PixelFeaturePass pass;
+//        PixelFeaturePass pass;
 
-        public override void Create()
-        {
-            pass = new PixelFeaturePass(settings.evt, settings.matBlit, settings.pixelDensity, settings.layerMask);
-        }
+//        public override void Create()
+//        {
+//            pass = new PixelFeaturePass(settings.evt, settings.matBlit, settings.pixelDensity, settings.layerMask);
+//        }
 
-        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
-        {
-            renderer.EnqueuePass(pass);
-        }
-    }
-}
+//        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+//        {
+//            renderer.EnqueuePass(pass);
+//        }
+//    }
+//}
